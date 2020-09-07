534136ef8c0be3a095f4a462ab5703427102a5e7
SugawaraKou <38384008+SugawaraKou@users.noreply.github.com>
2020-09-08 00:25:53 +0300

    Коменты
    Забыл прописать коменты

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot
 from datetime import datetime, timedelta
 
 
-def day_and_week_file_read(flag):
+def day_and_week_file_read(flag):  # Функция для обработки дня и недели
     day = datetime.today()
     week = int(datetime.now().strftime("%V")) % 2  # 1 - чётная 0 - не чётная
     if flag:
@@ -23,31 +23,34 @@ def day_and_week_file_read(flag):
     return file
 
 
-def calls():
+def calls():  # Функция для звоков
     file_open = open('timetable/calls', encoding='utf-8')
     file = file_open.read()
     file_open.close()
     return file
 
 
-bot = telebot.TeleBot(config.token)
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('сегодня', 'завтра', 'звонки', 'неделя')
+bot = telebot.TeleBot(config.token)  # Импортируе токе бота
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True, True, True,)  # Создаём клаву
+keyboard1.row('сегодня', 'завтра', 'звонки', 'неделя')  # Присваиваем имена кнопок
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
+@bot.message_handler(commands=['start'])  # При начале или команды /start
+def start_message(message):  # Функция для отправки приветсвия
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    # Отправляем сообщение и показываем клаву
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text'])  # При любом сообщении
 def send_text(message):
-    if message.text.lower() == 'сегодня':
+    if message.text.lower() == 'сегодня':  # Если в сообщение есть "сегодня" делаем
         flag = False
-        bot.send_message(message.chat.id, 'Расписание на сегодня\n' + day_and_week_file_read(flag), reply_markup=keyboard1)
+        bot.send_message(message.chat.id, 'Расписание на сегодня\n' + day_and_week_file_read(flag),
+                         reply_markup=keyboard1)
     elif message.text.lower() == 'завтра':
         flag = True
-        bot.send_message(message.chat.id, 'Расписание на завтра\n' + day_and_week_file_read(flag), reply_markup=keyboard1)
+        bot.send_message(message.chat.id, 'Расписание на завтра\n' + day_and_week_file_read(flag),
+                         reply_markup=keyboard1)
     elif message.text.lower() == 'звонки':
         bot.send_message(message.chat.id, 'Расписание звонков\n' + calls(), reply_markup=keyboard1)
     elif message.text.lower() == 'неделя':
@@ -57,4 +60,4 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Я не понял...', reply_markup=keyboard1)
 
 
-bot.polling()
+bot.polling()  # звкрываем бота
